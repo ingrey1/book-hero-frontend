8b09e820a4cd47141851fbe90bb8f51fb9101ced
github username <ingrey1@gmail.com>
2020-03-09 17:28:54 -0700

    added redux devtools middlware

--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
-import {createStore, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from "./reducers/root"
+import {composeWithDevTools} from 'redux-devtools-extension'
 
 
-const store = createStore(rootReducer, applyMiddleware(thunk))
+const store = createStore(rootReducer, applyMiddleware(composeWithDevTools(thunk))
 
 export default(store)
