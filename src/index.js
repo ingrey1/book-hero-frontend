deac2a2d28d57f7c76e3a6dd11b6255787bbf156
github username <ingrey1@gmail.com>
2020-03-09 17:37:51 -0700

    redux setup with middleware complete

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import {Provider} from 'react-redux';
 import {BrowserRouter as Router} from 'react-router-dom'
 import store from './store'
 
-
+debugger
 ReactDOM.render(
     <Provider store={store}>
       <Router>
--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
-import {createStore, applyMiddleware, compose} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from "./reducers/root"
 import {composeWithDevTools} from 'redux-devtools-extension'
 
 
-const store = createStore(rootReducer, applyMiddleware(composeWithDevTools(thunk))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)) )
 
-export default(store)
+export default store
