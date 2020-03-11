67e8b44a0adcfc1b0ed168bd01857b849ae21047
github username <ingrey1@gmail.com>
2020-03-11 15:28:04 -0700

    added router functionality to Main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import SignIn from './SignIn'
+import {BrowserRouter as Router, Switch, Redirect} from 'react-router-dom'
 
 
 function Main({signOut, signInWithGoogle}) {
     // routes declared here
-   
+   // <SignIn signInWithGoogle={signInWithGoogle} />
+   const loginFunctions = {
+       signInWithGoogle,
+       signOut
+   }
 
     return <div>
-        <SignIn signInWithGoogle={signInWithGoogle} />
+        <Router>
+         <Switch >   
+           <Route exact to="/welcome" render={(routerProps) => <Welcome {...routerProps} loginFunctions={loginFunctions}  /> }   />
+           <Route />
+           <Route />
+           <Route />
+           <Route to="*" component={<Redirect to="/welcome" />} />
+        </Switch>
+        </Router>
     </div>
 } 
 
