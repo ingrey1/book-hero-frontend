fd5a58f44b8a8416bbf1031d8117d98e8ce9d32d
github username <ingrey1@gmail.com>
2020-03-11 17:21:50 -0700

    added index.css and setup for react bootstrap

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom'
 import Welcome from './Welcome'
 import Home from "./Home"
+import Navigation from "./Navigation"
 
 function Main({signOut, signInWithGoogle, ...props}) {
    
@@ -11,6 +12,7 @@ function Main({signOut, signInWithGoogle, ...props}) {
 
     return <div>
         <Router>
+         <Navigation />
          <Switch >
          <Route exact path="/home" render={(routerProps) => <Home {...routerProps}  /> }   />       
          <Route exact path="/welcome" render={(routerProps) => <Welcome {...routerProps} loginFunctions={loginFunctions}  /> }   />  
---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavBar from 'react-bootstrap/Navbar'
+
+function Navigation(props) {
+
+    return <NavBar className="navbar">
+
+       <NavBar.Brand>
+           Book Hero
+       </NavBar.Brand>
+
+    </NavBar>
+
+}
+
+export default Navigation
