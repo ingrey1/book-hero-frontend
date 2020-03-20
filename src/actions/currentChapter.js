22f7dc03558deb6bb24a1ac372a5c8d717b7eab6
github username <ingrey1@gmail.com>
2020-03-20 09:55:58 -0700

    added redux types for updating chapter information

--- src/actions/currentChapter.js
+++ src/actions/currentChapter.js
@@ -1,4 +1,8 @@
-import {START_READER, END_READER, GET_CHAPTER} from './types'
+import {START_READER, END_READER,
+     SET_CURRENT_CHAPTER,
+      SET_NEXT_CHAPTER, 
+      SET_PREVIOUS_CHAPTER,
+      UPDATE_CHAPTER_LOCATION} from './types'
 
 function startReader() {
     return {
