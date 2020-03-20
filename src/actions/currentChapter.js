cd9aa42cc2ce192782f76bb751ef53ba7613cd9b
github username <ingrey1@gmail.com>
2020-03-20 10:34:42 -0700

    added thunk action creator retrieveCurrentChapter

--- src/actions/currentChapter.js
+++ src/actions/currentChapter.js
@@ -24,6 +24,27 @@ function setCurrentChapter(chapter) {
     }
 }
 
+function retrieveCurrentChapter(userId, bookId) {
+    
+    const token = localStorage.getItem('fire_token')
+    
+    return function(dispatch) {
+    
+        getCurrentChapter(userId, bookId, token).then(res => res.json()).then(data => {
+            if (!data.errors) { // all good, have chapter
+
+                dispatch(setCurrentChapter(data))
+
+            } else { // handle errors; display message to inform user couldnt retrieve current chapter
+
+            }
+        }).catch(err => console.log(err)) // handle errors 
+
+
+    }
+}
+
+
 function setNextChapter(chapter) {
 
     return {
