d499a33bec3abdc542f1363355c17b6e3140ec1b
github username <ingrey1@gmail.com>
2020-03-23 04:24:20 -0700

    added previous chapter button

--- src/components/Reader.js
+++ src/components/Reader.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react'
 import {connect} from 'react-redux'
 import {retrieveCurrentChapter, startReader, endReader, setNextChapter, setPreviousChapter} from '../actions/currentChapter'
 import {Row, Col, Container} from 'react-bootstrap'
+import {Button, Icon} from 'semantic-ui-react'
 
 
 
@@ -36,13 +37,28 @@ function Reader({books, match, currentChapter, retrieveCurrentChapter, setNextCh
         <Container style={readerContainerStyles}>
             
   <Row>
-    <Col></Col>
+    <Col> 
+    
+     {<Button>
+     
+      <Button.Content visible>
+        <Icon name='arrow left' />
+      </Button.Content>
+     </Button> }
+    
+    </Col>
     <Col style={readerStyles}>
     <h2 id="chapter-title">{currentChapter.content && currentChapter.title}</h2>
     <div id="chapter-content">{currentChapter.content && currentChapter.content.substring(currentChapter.current_word, currentChapter.current_word + max_characters)}</div>
     
     </Col>
-    <Col></Col>
+    <Col>
+     <Button>
+    <Button.Content visible>
+        <Icon name='arrow right' />
+      </Button.Content>
+    </Button>
+    </Col>
   </Row>
 </Container>
        
