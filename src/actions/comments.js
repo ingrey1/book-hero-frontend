5186e954171d3331f033723ded20ba9c3f1fe377
github username <ingrey1@gmail.com>
2020-03-26 08:44:36 -0700

    added add comment action creator

--- src/actions/comments.js
+++ src/actions/comments.js
@@ -1,5 +1,5 @@
 import {SET_COMMENTS} from './types'
-import {getComments} from '../api/api'
+import {getComments, createComment} from '../api/api'
 
 
 export const setComments = (comments) => {
@@ -9,11 +9,31 @@ export const setComments = (comments) => {
     }
 }
 
+export const addComment = (comment) => {
+    return {
+        type: ADD_COMMENT,
+        payload: comment
+    }
+}
+
 export const retrieveAndSetComments = (userId, bookId, chapterId) => {
     return function(dispatch) {
         getComments(localStorage.getItem('fire_token', userId, bookId, chapterId)).then(res => res.json()).then(comments => {
             if (!data.errors) {
                 dispatch(setComments(comments))
+            } else {
+                // handle errors
+            }
+           
+        }).catch(err => console.log(err))
+    }
+} 
+
+export const createAndSetComment = (userId, bookId, chapterId, content) => {
+    return function(dispatch) {
+        createComment(localStorage.getItem('fire_token', userId, bookId, chapterId, content)).then(res => res.json()).then(comment => {
+            if (!data.errors) {
+                dispatch(addComment(comment))
             } else {
                 // handle errors
             }
