e00d291009fb642c830239d45dafd4a9adcbc2fa
github username <ingrey1@gmail.com>
2020-03-26 08:54:13 -0700

    added handleCreateComment method to Comments component

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,8 +1,9 @@
 import React, {useState} from 'react'
 import {List, Form, TextArea, Button} from 'semantic-ui-react'
 import {connect} from 'react-redux'
+import {createAndSetComment} from '../actions/comments'
 
-function Comments({comments, ...props}) {
+function Comments({comments, userId, chapterId, createComment, ...props}) {
 
     const [showAddCommentState, setShowAddCommentState] = useState(false)
     const [commentTextState, setCommentTextState] = useState("")
@@ -11,33 +12,45 @@ function Comments({comments, ...props}) {
         setCommentTextState(e.target.value)
     }
 
-//     return <div><Button onClick={() => setShowAddCommentState(!showAddCommentState)} color="teal">Toggle Comment Box</Button> 
-//          {showAddCommentState &&  <Form>
-//     <TextArea value={commentTextState} onChange={handleCommentTextChange} placeholder='Write a comment' />
-//     <Button color="teal">Submit Comment</Button>
-//   </Form>}
-//         <List>
-//          <List.Header>Comments</List.Header>
-//          {comments.map(comment => {
+    const handleCreateComment = () => {
+      createComment(userId, bookId, chapterId, commentTextState)
+    }
+
+    return <div><Button onClick={() => setShowAddCommentState(!showAddCommentState)} color="teal">Toggle Comment Box</Button> 
+         {showAddCommentState &&  <Form>
+    <TextArea value={commentTextState} onChange={handleCommentTextChange} placeholder='Write a comment' />
+    <Button onClick={handleCreateComment} color="teal">Submit Comment</Button>
+  </Form>}
+        <List>
+         <List.Header>Comments</List.Header>
+         {comments.map(comment => {
          
-//          return <List.Item>
+         return <List.Item>
                
-//                <p>comment.content</p>
+               <p>comment.content</p>
                 
 
-//          </List.Item>})}
+         </List.Item>})}
 
-//     </List>
+    </List>
 
-//     </div>
+    </div>
     
 
- return <div>comments</div>
 }
 
 const mapStateToProps = state => {
     return {
-        comments: state.comments
+        comments: state.comments,
+        userId: state.auth.userId,
+        chapterId: state.currentChapter.id,
+        bookId: state.currentChapter.book_id
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        createComment: (userId, bookId, chapterId, content) => dispatch(createAndSetComment(userId, bookId, chapterId, content))
     }
 }
 
--- src/reducers/comments.js
+++ src/reducers/comments.js
@@ -6,6 +6,17 @@ const initialState = []
 export default function comments(state=initialState, action) {
     switch (action.type) {
 
+        case ADD_COMMENT:
+
+          return [
+              ...state,
+              action.payload
+          ]
+
+        case SET_COMMENTS:
+
+           return action.payload 
+
         default: return state
     }
 }
