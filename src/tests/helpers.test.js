00707502cee2bc878f10d7d54dd25b7fc8a46081
github username <ingrey1@gmail.com>
2020-05-29 15:22:38 -0700

    added another test for utilities/helpers#getBookByChapter

--- src/tests/helpers.test.js
+++ src/tests/helpers.test.js
@@ -6,15 +6,38 @@ import {getBookByChapter } from '../utilities/helpers'
 //     return book
 //  }
 describe('getBookByChapter', () => {
+
+
+    const book = {id: 7}
+    const books = [book, {id: 9}, {id: 42}]
+    const emptyBooks = []
+
     it('correct book from array with multiple books', () => {
         // characterize expected result
         // use function you're testing to get actual result
         // compare expected result and the actual result
-        const book = {id: 7} 
-        const books = [book, {id: 9}, {id: 42}]
+        
+        
         const bookId = 7
         const expectedResult = book
         const actualResult = getBookByChapter(books, bookId)
         expect(actualResult).toEqual(expectedResult)  
     })
+    
+    it('should return undefined if book isnt in books', () => {
+       const bookId = 50
+       const expectedResult = undefined
+       const actualResult = getBookByChapter(books, bookId) 
+       expect(actualResult).toEqual(expectedResult)      
+    })
+
+    it('should return undefined if books is empty', () => {
+
+        const bookId = 1
+        const expectedResult = undefined
+        const actualResult = getBookByChapter(emptyBooks, bookId)
+        expect(actualResult).toEqual(expectedResult)
+    })
+
+
 })
