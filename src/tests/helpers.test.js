3df51f08f2033093892442dced97cdae80a70153
github username <ingrey1@gmail.com>
2020-05-29 15:08:14 -0700

    added unit test for getBookByChapter

--- src/tests/helpers.test.js
+++ src/tests/helpers.test.js
@@ -1,6 +1,20 @@
-import {getChapterByBook } from '../utilities/helpers'
+import {getBookByChapter } from '../utilities/helpers'
 
-
-describe('getChapterByBook', () => {
-    
+// export const getBookByChapter = (books, book_id) => {
+  
+//     const book = books.find(book => book.id === book_id)
+//     return book
+//  }
+describe('getBookByChapter', () => {
+    it('correct book from array with multiple books', () => {
+        // characterize expected result
+        // use function you're testing to get actual result
+        // compare expected result and the actual result
+        const book = {id: 7} 
+        const books = [book, {id: 9}, {id: 42}]
+        const bookId = 7
+        const expectedResult = book
+        const actualResult = getBookByChapter(books, bookId)
+        expect(actualResult).toEqual(expectedResult)  
+    })
 })
