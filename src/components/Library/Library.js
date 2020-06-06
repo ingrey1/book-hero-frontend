1412e8746b752ed9964d9e6b0b93442435758e28
github username <ingrey1@gmail.com>
2020-06-06 05:10:24 -0700

    added library.css

--- src/components/Library/Library.js
+++ src/components/Library/Library.js
@@ -4,6 +4,7 @@ import LibraryBooks from '../LibraryBooks/LibraryBooks'
 import LibraryControls from '../LibraryControls/LibraryControls'
 import {retrieveLibrary} from '../../actions/library'
 import {clearCurrentChapter} from '../../actions/currentChapter'
+import "./Library.css"
 
 
 function Library({getLibrary, clearCurrentChapter, library, userId, loggedIn, ...props}) {
