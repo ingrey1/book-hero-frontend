c28f73421387ea3b90a7f60857d53749f38badfa
coronaisme <coronaisme@gmail.com>
2020-06-08 11:40:31 -0700

    prettied, got book images on libraryListBook

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,63 +1,61 @@
-import React, { useEffect } from 'react';
-import {connect} from 'react-redux'
-import {Card} from 'semantic-ui-react'
-import {retrieveLibrary} from '../../actions/library'
-import {retrieveAndSetBooks} from '../../actions/books'
-import {clearCurrentChapter} from '../../actions/currentChapter'
-import LibraryListBook from '../LibraryListBook/LibraryListBook'
-import "./Home.css"
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { Card } from "semantic-ui-react";
+import { retrieveLibrary } from "../../actions/library";
+import { retrieveAndSetBooks } from "../../actions/books";
+import { clearCurrentChapter } from "../../actions/currentChapter";
+import LibraryListBook from "../LibraryListBook/LibraryListBook";
+import "./Home.css";
 
-const colStyle = {
-    marginTop: '25px'
-}
+// const colStyle = {
+//     marginTop: '25px'
+// }
 
 const titleStyle = {
-    marginTop: '25px'
+  marginTop: "25px",
+};
+
+function Home({
+  userId,
+  books,
+  setBooks,
+  getLibrary,
+  clearCurrentChapter,
+  ...props
+}) {
+  useEffect(() => {
+    setBooks(userId);
+    getLibrary(userId);
+    clearCurrentChapter();
+  }, []);
+
+  return (
+    <div>
+      <h2 style={titleStyle}>Top Picks</h2>
+
+      <Card.Group>
+        {books &&
+          books.map((book) => (
+            <LibraryListBook key={book.id} topPick={true} book={book} />
+          ))}
+      </Card.Group>
+    </div>
+  );
 }
-
-function Home({userId, books, setBooks, getLibrary, clearCurrentChapter, ...props}) {
-
-
-    useEffect(() => {
-        setBooks(userId)
-        getLibrary(userId)
-        clearCurrentChapter()
- }, [])
-
-         return <div>
-             
-             <h2 style={titleStyle}>Top Picks</h2>
-
-             <Card.Group>
-                  
-                  
-                         {books && books.map(book => <LibraryListBook topPick={true} book={book} /> )}
-
-                  </Card.Group></div>
-               
-          
-            
-    
-            
-}
-
 
 const mapStateToProps = (state) => {
-   return {
-       userId: state.auth.userId,
-       books: state.books
-   }
-}
+  return {
+    userId: state.auth.userId,
+    books: state.books,
+  };
+};
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getLibrary: (userId) => dispatch(retrieveLibrary(userId)),
+    clearCurrentChapter: () => dispatch(clearCurrentChapter()),
+    setBooks: (userId) => dispatch(retrieveAndSetBooks(userId)),
+  };
+};
 
-   return {
-
-     getLibrary: (userId) => dispatch(retrieveLibrary(userId)),
-     clearCurrentChapter: () => dispatch(clearCurrentChapter()),
-     setBooks: (userId) => dispatch(retrieveAndSetBooks(userId))
-       
-   }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home)
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
--- src/components/LibraryListBook/LibraryListBook.js
+++ src/components/LibraryListBook/LibraryListBook.js
@@ -1,31 +1,36 @@
-import React from 'react'
-import {Card, Image} from 'semantic-ui-react'
-import {Link} from 'react-router-dom'
-import "./LibraryListBook.css"
+import React from "react";
+import { Card, Image } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import "./LibraryListBook.css";
 
-const bookContainerStyle =  {
-  margin: '25px' 
-}
+const bookContainerStyle = {
+  margin: "25px",
+};
 
+function LibraryListBook({ book, topPick, ...props }) {
+  // console.log(book, "book")
+  //debugger
 
-function LibraryListBook({book, topPick, ...props }) {
-    console.log("book", book)
-    //debugger 
-    return <Link style={bookContainerStyle} to={topPick ? `/top_picks/${book.id}`:`/library/${book.id}`}><Card style={{ width: '15rem', height: '20rem' }}>
-    <Image  style={{height: '10rem'}} src={book.imageUrl} />
-    <Card.Content>
-      <Card.Header>{book.title}</Card.Header>
-      
-<Card.Description>{book.authors ? book.authors.map(a => `${a} ` ): book.author.name}</Card.Description>
-      
-      
-      </Card.Content>
+  return (
 
-      
     
-  </Card>
-  </Link>
 
+    <Link
+      style={bookContainerStyle}
+      to={topPick ? `/top_picks/${book.id}` : `/library/${book.id}`}
+    >
+      <Card style={{ width: "15rem", height: "20rem" }}>
+        <Image style={{ height: "10rem" }} src={book.image_url} />
+        <Card.Content>
+          <Card.Header>{book.title}</Card.Header>
+
+          <Card.Description>
+            {book.authors ? book.authors.map((a) => `${a} `) : book.author.name}
+          </Card.Description>
+        </Card.Content>
+      </Card>
+    </Link>
+  );
 }
 
-export default LibraryListBook
+export default LibraryListBook;
