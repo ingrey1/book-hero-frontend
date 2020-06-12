978132d194e828c5326e271be855bb3ef6fe30c5
github username <ingrey1@gmail.com>
2020-06-12 08:41:32 -0700

    completed styling for Welcome view

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,67 +1,87 @@
-import React, {useState} from 'react'
-import {connect} from 'react-redux'
-import {loginWithGoogle, createUserWithEmailPassword} from '../../actions/auth'
-import { Button, Form, Grid, Header, Image, Message, Segment, Icon } from 'semantic-ui-react'
-import "./SignIn.css"
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import {
+  loginWithGoogle,
+  createUserWithEmailPassword
+} from "../../actions/auth";
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Image,
+  Message,
+  Segment,
+  Icon
+} from "semantic-ui-react";
+import "./SignIn.css";
 
+function SignIn({
+  providers,
+  user,
+  firebaseAppAuth,
+  loginWithGoogle,
+  loginFunctions: {
+    signInWithGoogle,
+    signInWithEmailAndPassword,
+    createUserWithEmailAndPassword
+  },
+  createUser,
+  ...props
+}) {
+  const [credentialsState, setCredentialsState] = useState({
+    email: "",
+    password: "",
+    passwordVerification: ""
+  });
+  const [signupState, setSignupState] = useState(false);
+  const [errorsState, setErrorsState] = useState({
+    email: false,
+    password: false,
+    passwordVerification: false
+  });
 
-
-
-
-const componentStyles = {
-
-  backgroundImage: `url(${process.env.PUBLIC_URL}/images/background_book1.jpg)`
-
-}
-
-
-
-function SignIn({providers, user, firebaseAppAuth, loginWithGoogle, loginFunctions: {signInWithGoogle, signInWithEmailAndPassword,createUserWithEmailAndPassword}, createUser, ...props}) {
-  
-
-
-
-  const [credentialsState, setCredentialsState] = useState({email: "", password: "", passwordVerification: ""})
-  const [signupState, setSignupState] = useState(false)
-  const [errorsState, setErrorsState] = useState({email: false, password: false, passwordVerification: false})
-  
   // IN REDUX HANDLE MESSAGES FOR FAILED SIGNUP AND FAILED LOGIN
 
-
   const validSignup = () => {
-    return validEmail() && validPassword() && validPasswordVerification()
-  }
+    return validEmail() && validPassword() && validPasswordVerification();
+  };
 
   const validEmail = () => {
-      return credentialsState.email.includes('@')
-  }
+    return credentialsState.email.includes("@");
+  };
 
   const validPassword = () => {
-    return credentialsState.password.length > 6
-  }
+    return credentialsState.password.length > 6;
+  };
 
   const validPasswordVerification = () => {
-    return credentialsState.passwordVerification.length === credentialsState.password.length
-  }
+    return (
+      credentialsState.passwordVerification.length ===
+      credentialsState.password.length
+    );
+  };
 
-  const handleChange = (e) => {
-    const value = e.target.value
-    const name = e.target.name
-    setCredentialsState({...credentialsState, [name]: value})
-  }
-
- 
+  const handleChange = e => {
+    const value = e.target.value;
+    const name = e.target.name;
+    setCredentialsState({ ...credentialsState, [name]: value });
+  };
 
   const handleLogin = () => {
     setErrorsState({
       ...errorsState,
       password: !validPassword(),
       email: !validEmail()
-    })  
-      if (validEmail() && validPassword()) loginWithGoogle(signInWithEmailAndPassword, credentialsState.email, credentialsState.password, firebaseAppAuth)
-     
-     
-  }
+    });
+    if (validEmail() && validPassword())
+      loginWithGoogle(
+        signInWithEmailAndPassword,
+        credentialsState.email,
+        credentialsState.password,
+        firebaseAppAuth
+      );
+  };
 
   const handleSignup = () => {
     setErrorsState({
@@ -69,74 +89,127 @@ function SignIn({providers, user, firebaseAppAuth, loginWithGoogle, loginFunctio
       password: !validPassword(),
       email: !validEmail(),
       passwordVerification: !validPasswordVerification()
-      
-    })
-    console.log("password", credentialsState.password)
-    console.log("password verification", credentialsState.passwordVerification)
-      if (validSignup()){
-        console.log("handlesignup called")
-        createUser(createUserWithEmailAndPassword, credentialsState.email, credentialsState.password)
-      }
-      
-  }
+    });
+    console.log("password", credentialsState.password);
+    console.log("password verification", credentialsState.passwordVerification);
+    if (validSignup()) {
+      console.log("handlesignup called");
+      createUser(
+        createUserWithEmailAndPassword,
+        credentialsState.email,
+        credentialsState.password
+      );
+    }
+  };
 
- 
-  return <div style={componentStyles}>
-    <Grid textAlign='center' style={{ height: '550px' }} verticalAlign='middle'>
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header as='h2' color='blue' textAlign='center'>
-        <Image src={process.env.PUBLIC_URL + '/images/logo.svg'} /> {signupState ? "Signup" : "Login"}
-      </Header>
-      <Form size='large'>
-        <Segment stacked>
-          <Form.Input error={errorsState.email ?  {content:'Please enter a valid email', pointing: 'below' }: null} onChange={handleChange} fluid icon='user' name="email" iconPosition='left' placeholder='E-mail address' value={credentialsState.email} />
-          <Form.Input error={errorsState.password ?  {content:'Password must be at least 6 characters', pointing: 'below' }: null} onChange={handleChange}
-            name="password"
-            value={credentialsState.password} 
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Password'
-            type='password'
-           
-          />
-          {signupState && <Form.Input
-            error={errorsState.passwordVerification ?  {content:'Make sure your passwords match', pointing: 'below' }: null}
-            onChange={handleChange}
-            name="passwordVerification"
-            value={credentialsState.passwordVerification} 
-            fluid
-            icon='lock'
-            iconPosition='left'
-            placeholder='Verify Password'
-            type='password'
-          /> }
-
-          <Button onClick={signupState ? handleSignup : handleLogin  } color='blue' fluid size='large'>
+  return (
+    <div>
+      <Grid
+        textAlign="center"
+        style={{ height: "550px" }}
+        verticalAlign="middle"
+      >
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" textAlign="center" className="secondary-color-text">
+            <Image src={require("../../assets/images/logo.svg")} />{" "}
             {signupState ? "Signup" : "Login"}
-          </Button>
-        </Segment>
-      </Form>
-      <Message>
-      <Button style={{marginRight: '20px'}} onClick={() => loginWithGoogle(signInWithGoogle)} color='google plus'>
-      <Icon name='google' /> Sign in With Google
-    </Button>
-      <a onClick={() => setSignupState(!signupState)}>{signupState ? "Login": "Signup"}</a>
-        
-      </Message>
-    </Grid.Column>
-  </Grid>
-      
-  </div>
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input
+                error={
+                  errorsState.email
+                    ? {
+                        content: "Please enter a valid email",
+                        pointing: "below"
+                      }
+                    : null
+                }
+                onChange={handleChange}
+                fluid
+                icon="user"
+                name="email"
+                iconPosition="left"
+                placeholder="E-mail address"
+                value={credentialsState.email}
+              />
+              <Form.Input
+                error={
+                  errorsState.password
+                    ? {
+                        content: "Password must be at least 6 characters",
+                        pointing: "below"
+                      }
+                    : null
+                }
+                onChange={handleChange}
+                name="password"
+                value={credentialsState.password}
+                fluid
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                type="password"
+              ></Form.Input>
+              {signupState && (
+                <Form.Input
+                  error={
+                    errorsState.passwordVerification
+                      ? {
+                          content: "Make sure your passwords match",
+                          pointing: "below"
+                        }
+                      : null
+                  }
+                  onChange={handleChange}
+                  name="passwordVerification"
+                  value={credentialsState.passwordVerification}
+                  fluid
+                  icon="lock"
+                  iconPosition="left"
+                  placeholder="Verify Password"
+                  type="password"
+                />
+              )}
+
+              <Button
+                onClick={signupState ? handleSignup : handleLogin}
+                className="login-button"
+                fluid
+                size="large"
+              >
+                {signupState ? "Signup" : "Login"}
+              </Button>
+            </Segment>
+          </Form>
+          <Message>
+            <Button
+              style={{ marginRight: "20px" }}
+              onClick={() => loginWithGoogle(signInWithGoogle)}
+              className="google-button"
+            >
+              <Icon name="google" /> Sign in With Google
+            </Button>
+            <a
+              className="toggle-signup-link"
+              onClick={() => setSignupState(!signupState)}
+            >
+              {signupState ? "Login" : "Signup"}
+            </a>
+          </Message>
+        </Grid.Column>
+      </Grid>
+    </div>
+  );
 }
 
-const mapDispatchToProps = (dispatch) => {
-
+const mapDispatchToProps = dispatch => {
   return {
-    loginWithGoogle: (f, email, password, authy) => dispatch(loginWithGoogle(f, email, password, authy)),
-    createUser: (f, email, password) => dispatch(createUserWithEmailPassword(f, email, password)) 
-  }
+    loginWithGoogle: (f, email, password, authy) =>
+      dispatch(loginWithGoogle(f, email, password, authy)),
+    createUser: (f, email, password) =>
+      dispatch(createUserWithEmailPassword(f, email, password))
+  };
+};
 
-}
-
-export default connect(null, mapDispatchToProps)(SignIn)
+export default connect(null, mapDispatchToProps)(SignIn);
